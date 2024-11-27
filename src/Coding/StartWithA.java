56412a424df65ce6b9964423d26bbb618dc85e64
kadapala sivaji <sivajisivaji68@gmail.com>
2024-11-27 20:45:11 +0530

    interview

--- src/Coding/StartWithA.java
+++ src/Coding/StartWithA.java
@@ -2,6 +2,7 @@ package InterviewsCoding;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StartWithA {
     public static void main(String[] args) {
@@ -9,5 +10,7 @@ public class StartWithA {
         names.stream()
                 .filter(name -> name.startsWith("A"))
                 .forEach(System.out::println);
+        List<String> lists = Arrays.asList("Alice", "2ob", "3harlie");
+        System.out.println (lists.stream ().filter ( i->i.matches ("^[0-9].*")).collect( Collectors.toList()));
     }
 }
--- src/CoreJava/Interface/DiamondProblemResolution.java
+++ src/CoreJava/Interface/DiamondProblemResolution.java
@@ -26,6 +26,6 @@ public class DiamondProblemResolution implements Interface1,Interface2 {
     public static void main(String[] args) {
         DiamondProblemResolution obj=new DiamondProblemResolution ();
         obj.method1 ();
-        obj.print ();
+//        obj.print ();
     }
 }
